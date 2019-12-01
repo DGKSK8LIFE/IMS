075fe84b2e8949350f9869c4a63531033a7730b3
Tarek Ali <tarekali15@outlook.com>
2019-12-01 15:48:36 -0600

    fixed frontend so that forms now submit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -34,6 +35,7 @@ func handleCreateSite(w http.ResponseWriter, r *http.Request) {
 func userAuth(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
+	fmt.Println(username, password)
 	if len(username) > 0 && len(password) > 0 {
 		/* here we'll query the user account database to see if the this uname/password
 		pair is a valid row */
