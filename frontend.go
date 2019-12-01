37cdc0ba6dc5dc33804320e6408a2a82bf73b663
Tarek Ali <tarekali15@outlook.com>
2019-12-01 12:16:42 -0600

    added go script

---
+++ frontend.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var (
+	loginSite  *template.Template
+	createSite *template.Template
+)
+
+func init() {
+	loginSite = template.Must(template.ParseGlob("login.html"))
+	createSite = template.Must(template.ParseGlob("create.html"))
+}
+
+func main() {
+	http.HandleFunc("/", handleLoginSite)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handleLoginSite(w http.ResponseWriter, r *http.Request) {
+	loginSite.Execute(w, "login.html")
+}
