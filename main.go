b325557a5257ce3411fdc2e1f196f369307a47a8
Tarek Ali <tarekali15@outlook.com>
2019-12-01 19:11:30 -0600

    adding login authentification with sqlite and change placeholder/textbox names

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -28,19 +31,25 @@ func main() {
 }
 
 func userAuth(w http.ResponseWriter, r *http.Request) {
-	username := r.FormValue("username")
+	email := r.FormValue("email")
 	password := r.FormValue("password")
-	if len(username) > 0 && len(password) > 0 {
+	if len(email) > 0 && len(password) > 0 {
+		db, err := sql.Open("sqlite3", "accounts.sqlite")
+		if err != nil {
+			log.Fatal(err)
+		}
+		query := fmt.Sprintf("SELECT * FROM accounts WHERE email='%s'", email)
+		isValidAccount := db.Exec(query)
 		/* here we'll query the user account database to see if the this uname/password
 		pair is a valid row */
 	} // else {output some kind of message saying to fill out all forms}
 }
 
 func createAccount(w http.ResponseWriter, r *http.Request) {
-	username := r.FormValue("username")
+	email := r.FormValue("username")
 	password := r.FormValue("password")
 	confirm := r.FormValue("confirm")
-	if len(username) > 0 && len(password) > 0 && len(confirm) > 0 && password == confirm {
+	if len(email) > 0 && len(password) > 0 && len(confirm) > 0 && password == confirm {
 		/* here we check if the account already exists, if so, return an error message
 		if not, write the row to the account database
 		*/
