f22163376e86d7384a4ea2424e0dd923a4f5e55b
Tarek Ali <tarekali15@outlook.com>
2019-12-02 13:09:04 -0600

    added redundant semicolons

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func createAccount(w http.ResponseWriter, r *http.Request) {
 
 func rowExists(email, password string, db *sql.DB) bool {
 	var exists bool
-	query := fmt.Sprintf("SELECT * FROM ACCOUNTS WHERE email='%s' AND password='%s'", email, password)
+	query := fmt.Sprintf("SELECT * FROM ACCOUNTS WHERE email='%s' AND password='%s';", email, password)
 	if err := db.QueryRow(query).Scan(&email, &password); err != nil && err != sql.ErrNoRows {
 		log.Fatalf("database error, we're fucked")
 	} else if err == sql.ErrNoRows {
@@ -92,7 +92,7 @@ func rowExists(email, password string, db *sql.DB) bool {
 
 func accountTaken(email string, db *sql.DB) bool {
 	var exists bool
-	query := fmt.Sprintf("SELECT * FROM ACCOUNTS WHERE email='%s'", email)
+	query := fmt.Sprintf("SELECT * FROM ACCOUNTS WHERE email='%s';", email)
 	if err := db.QueryRow(query).Scan(&email); err != nil && err != sql.ErrNoRows {
 		log.Fatalf("database error, we're fucked")
 	} else if err == sql.ErrNoRows {
